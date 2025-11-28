b3af7550c2d23e892ffc2412a2490aeeed819f23
Shubham Bhisikar <shubham.bhisikar@ecolibrium.io>
2025-11-28 23:14:57 +0530

    Status

---
+++ Demo Test 1.java
@@ -0,0 +1,18 @@
+package oOOPS;
+
+public interface Interface2 {
+	
+	
+	
+	void interface21(String age);
+	
+	
+	void interface22(int number);
+	
+	
+	static void Testmethod() 
+	{
+		System.out.println("TestMethods of Interface 2");
+	}
+
+}
